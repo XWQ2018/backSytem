99b76aaa95c3f35b78386d22393b6538e9f018a5
XWQ2018 <18476969365@163.com>
2019-10-12 15:33:08 +0800

    333

--- src/router/index.js
+++ src/router/index.js
@@ -4,133 +4,137 @@ Vue.use(VueRouter);
 
 //引入组件
 import Home from '@/components/Home';
-import Login from '@/components/Login';
-import productList from '@/components/productList';
-import goodsList from '@/components/goodsList';
-import updateMm from '@/components/updateMm';
-import updateZl from '@/components/updateZl';
-import toUser from '@/components/toUser';
-import addUser from '@/components/addUser';
-import editUser from '@/components/editUser';
-import addProductList from '@/components/addProductList';
-import editProductList from '@/components/editProductList';
-import addGoodsList from '@/components/addGoodsList';
-import editGoodsList from '@/components/editGoodsList';
+// import Login from '@/components/Login';
+// import productList from '@/components/productList';
+// import goodsList from '@/components/goodsList';
+// import updateMm from '@/components/updateMm';
+// import updateZl from '@/components/updateZl';
+// import toUser from '@/components/toUser';
+// import addUser from '@/components/addUser';
+// import editUser from '@/components/editUser';
+// import addProductList from '@/components/addProductList';
+// import editProductList from '@/components/editProductList';
+// import addGoodsList from '@/components/addGoodsList';
+// import editGoodsList from '@/components/editGoodsList';
+
+/* 另外按需加载的引入方式 */
+// const Login = () => import("@/components/Login");
+
 
 //配置路由
 const routes = [
-  {
-    path:'/home',
-    name:'Home',
-    component:Home,
-    meta: { requiresAuth: true },
-    children:[
-      {
-        path:'productList',
-        name:'productList',
-        component:resolve=>require(['@/components/productList'],resolve),
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home,
         meta: { requiresAuth: true },
-        children:[
-          
+        children: [
+            {
+                path: 'productList',
+                name: 'productList',
+                component: resolve => require(['@/components/productList'], resolve),
+                meta: { requiresAuth: true },
+                children: [
+
+                ]
+            },
+            {
+                path: 'goodsList',
+                name: 'goodsList',
+                component: resolve => require(['@/components/goodsList'], resolve),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: 'toUser',
+                name: 'toUser',
+                component: resolve => require(['@/components/toUser'], resolve),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: 'addUser',
+                name: 'addUser',
+                component: resolve => require(['@/components/addUser'], resolve),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: 'updateMm',
+                name: 'updateMm',
+                component: resolve => require(['@/components/updateMm'], resolve),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: 'updateZl',
+                name: 'updateZl',
+                component: resolve => require(['@/components/updateZl'], resolve),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: 'editUser',
+                name: 'editUser',
+                component: resolve => require(['@/components/editUser'], resolve),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: '/addProductList',
+                name: 'addProductList',
+                component: resolve => require(['@/components/addProductList'], resolve),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: '/editProductList',
+                name: 'editProductList',
+                component: resolve => require(['@/components/editProductList'], resolve),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: '/addGoodsList',
+                name: 'addGoodsList',
+                component: resolve => require(['@/components/addGoodsList'], resolve),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: '/editGoodsList',
+                name: 'editGoodsList',
+                component: resolve => require(['@/components/editGoodsList'], resolve),
+                meta: { requiresAuth: true },
+            },
+            {
+                path: '/dingdangl',
+                name: 'dingdangl',
+                component: resolve => require(['@/components/dingdangl'], resolve),
+            }
         ]
-      },
-      {
-        path:'goodsList',
-        name:'goodsList',
-        component:resolve=>require(['@/components/goodsList'],resolve),
-        meta: { requiresAuth: true },
-      },
-      {
-        path:'toUser',
-        name:'toUser',
-        component:resolve=>require(['@/components/toUser'],resolve),
-        meta: { requiresAuth: true },
-      },
-      {
-        path:'addUser',
-        name:'addUser',
-        component:resolve=>require(['@/components/addUser'],resolve),
-        meta: { requiresAuth: true },
-      },
-      {
-        path:'updateMm',
-        name:'updateMm',
-        component:resolve=>require(['@/components/updateMm'],resolve),
-        meta: { requiresAuth: true },
-      },
-      {
-        path:'updateZl',
-        name:'updateZl',
-        component:resolve=>require(['@/components/updateZl'],resolve),
-        meta: { requiresAuth: true },
-      },
-      {
-        path:'editUser',
-        name:'editUser',
-        component:resolve=>require(['@/components/editUser'],resolve),
-        meta: { requiresAuth: true },
-      },
-      {
-        path:'/addProductList',
-        name:'addProductList',
-        component:resolve=>require(['@/components/addProductList'],resolve),
-        meta: { requiresAuth: true },
-      },
-      {
-        path:'/editProductList',
-        name:'editProductList',
-        component:resolve=>require(['@/components/editProductList'],resolve),
-        meta: { requiresAuth: true },
-      },
-      {
-        path:'/addGoodsList',
-        name:'addGoodsList',
-        component:resolve=>require(['@/components/addGoodsList'],resolve),
-        meta: { requiresAuth: true },
-      },
-      {
-        path:'/editGoodsList',
-        name:'editGoodsList',
-        component:resolve=>require(['@/components/editGoodsList'],resolve),
-        meta: { requiresAuth: true },
-      },
-      {
-        path:'/dingdangl',
-        name:'dingdangl',
-        component:resolve=>require(['@/components/dingdangl'],resolve),
-      }
-    ]
-  },
-  {
-    path:'/login',
-    name:'Login',
-    component:resolve=>require(['@/components/Login'],resolve),
-  },
-  {
-    path:'/',
-    redirect:'/login'
-  }, //默认跳转首页的路由
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: resolve => require(['@/components/Login'], resolve),
+    },
+    {
+        path: '/',
+        redirect: '/login'
+    }, //默认跳转首页的路由
 ]
 
 //实例化路由
 const router = new VueRouter({
-  routes
+    routes
 })
 
 //路由拦截(全局)
-router.beforeEach((to,from,next)=>{
-  // console.log(sessionStorage.getItem('token'))
-  if(to.meta.requiresAuth){
-    if(sessionStorage.getItem('name')){
-      next()
-    }else{
-      next({
-        path:'/login'
-      })
+router.beforeEach((to, from, next) => {
+    // console.log(sessionStorage.getItem('token'))
+    if (to.meta.requiresAuth) {
+        if (sessionStorage.getItem('name')) {
+            next()
+        } else {
+            next({
+                path: '/login'
+            })
+        }
+    } else {
+        next()
     }
-  }else{
-    next()
-  }
 })
 //暴露路由
 export default router;
